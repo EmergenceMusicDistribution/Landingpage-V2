9db633463d518fc45a27b28f16cf40b8bfbb4e01
Baudwin <baudwin12@gmail.com>
2024-09-19 08:33:30 +0100

    latest commit

--- src/pages/home/components/FeaturesCard.jsx
+++ src/pages/home/components/FeaturesCard.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 
 export const FeaturesCard = ({title,description,image}) => {
   return (
--- src/pages/home/components/Section2.jsx
+++ src/pages/home/components/Section2.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 
 export const Section2 = () => {
   return (
